d2855ab952367ca45a3c636b9126f7d859e8b475
Emma Paris <eparis@atlassian.com>
2022-03-09 09:19:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Rachel-week2
+#### {This is a link to my week two independent project at Moringa School.}, {8th March 2022}
+#### By **{Rachel Namiba}**
+## Description
+{THis is a web application that allows users to enter their birth date, month and year as well as their gender. It then outputs their Akan names after calculating the actual day of the week they were born }
+## Setup/Installation requirements
+* Git clone the work
+* Navigate the directory
+* run the index.html file to view the webpage 
+* Enter the required data for your birth date and gender to see your Akan name.
+## Known Bugs
+{At the moment there are no errors but the application is not complete enough to achieve what it was intended to do.}
+## Technologies Used
+{The application was done using HTML, CSS, Javascript and Bootstrap}
+## Support and contact details
+{please reach me out for any contribution of codes or enquiries at 0154762309}
+*{MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.}*
+Copyright (c) {2022} **{Rachel Namiba}**
---
+++ app.js
@@ -0,0 +1,118 @@
+function getName(event) {
+
+    event.preventDefault();
+    
+    var femaleNames = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    var inputDay = document.getElementById("day").value;
+    var inputMonth = document.getElementById("month").value;
+    var inputYear = document.getElementById("year").value;
+    var inputCentury = document.getElementById("year").value;
+    var genders = document.getElementsByName('gender');
+
+    var dd = inputDay;
+    var mm = inputMonth;
+    var yy = inputYear.slice(2,4);
+    var cc = inputCentury.slice(0,2);
+
+    /* 
+        formula to calculate the day of the week
+
+        (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD) mod 7
+     */
+
+    var extDay = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
+
+
+    function getGender () {
+        for (let gender of genders){
+          if (gender.checked){
+            return gender.value;
+          }
+        }
+      }      
+
+    let genderData = getGender();
+
+    var originalDay = weekDays[extDay.toFixed()];
+    
+
+    if (genderData == "male") {
+        if (originalDay === 'Sunday') {
+            alert('born on a sunday your akan name is ' + maleNames[0]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + maleNames[0];
+            return true;
+        }
+        else if (originalDay === 'Monday') {
+            alert('born on a monday your akan name is ' + maleNames[1]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + maleNames[1];
+            return true;
+        }
+        else if (originalDay === 'Tuesday') {
+            alert('born on a tuesday your akan name is ' + maleNames[2]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + maleNames[2];
+            return true;
+        }
+        else if (originalDay === 'Wednesday') {
+            alert('born on a wednesday your akan name is ' + maleNames[3]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + maleNames[3];
+            return true;
+        }
+        else if (originalDay === 'Thursday') {
+            alert('born on a thursday your akan name is ' + maleNames[4]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + maleNames[4];
+            return true;
+        }
+        else if (originalDay === 'Friday') {
+            alert('born on a friday your akan name is ' + maleNames[5]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + maleNames[5];
+            return true;
+        }
+        else if (originalDay === 'Saturday') {
+            alert('born on a saturday your akan name is ' + maleNames[6]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + maleNames[6];
+            return true;
+        }
+    }
+    
+    if (genderData == "female") {
+        if (originalDay === 'Sunday') {
+            alert('born on a sunday your akan name is ' + femaleNames[0]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + femaleNames[0];
+            return true;
+        }
+        else if (originalDay === 'Monday') {
+            alert('born on a monday your akan name is ' + femaleNames[1]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + femaleNames[1];
+            return true;
+        }
+        else if (originalDay === 'Tuesday') {
+            alert('born on a tuesday your akan name is ' + femaleNames[2]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + femaleNames[2];
+            return true;
+        }
+        else if (originalDay === 'Wednesday') {
+            alert('born on a wednesday your akan name is ' + femaleNames[3]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + femaleNames[3];
+            return true;
+        }
+        else if (originalDay === 'Thursday') {
+            alert('born on a thursday your akan name is ' + femaleNames[4]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + femaleNames[4];
+            return true;
+        }
+        else if (originalDay === 'Friday') {
+            alert('born on a friday your akan name is ' + femaleNames[5]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + femaleNames[5];
+            return true;
+        }
+        else if (originalDay === 'Saturday') {
+            alert('born on a saturday your akan name is ' + femaleNames[6]);
+            document.getElementById("result").textContent = "Born on Sunday: Your Name is: " + femaleNames[6];
+            return true;
+        }
+    }
+    
+}
